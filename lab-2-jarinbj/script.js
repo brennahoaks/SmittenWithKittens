7bf16f7d7a77115eb5f47e68f6e5df1a8380e497
jarinbj <36678114+jarinbj@users.noreply.github.com>
2019-02-15 20:06:45 -0700

    Delete script.js

--- lab-2-jarinbj/script.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById("weatherSubmit").addEventListener("click", function(event) {
-    event.preventDefault();
-    const value = document.getElementById("weatherInput").value;
-    if (value === "")
-      return;
-    console.log(value);
-  });
